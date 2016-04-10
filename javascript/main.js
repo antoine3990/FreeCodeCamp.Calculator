82fb2892da7e8190a4ea24bc842baa2cc6a12ea4
Antoine <antoinecoulombe@Antoines-MacBook-Pro.local>
2016-04-10 10:57:05 -0400

    Added .keypress method to filter input on textbox.

--- javascript/main.js
+++ javascript/main.js
@@ -9,6 +9,11 @@ $(document).ready(function () {
     $("body").append('<div id="calculator"></div>');
     setCalculatorPos();
     setCalculator();
+    
+    $("#eq").keypress(function(e) {
+        filterInput("eq", String.fromCharCode(e.keyCode));
+        return false;
+    });
 });
 
 window.onresize = function(event) {
@@ -17,12 +22,19 @@ window.onresize = function(event) {
 
 // Calculator
 function setCalculator() {
-    var $calc = $("calculator");
-    var textbox = document.createElement("input");
-    document.getElementById("calculator").appendChild(textbox);
+    var calc = document.getElementById("calculator");
+    
+    $(calc).append('<input type="text" id="eq"/>');
 }
 
 function setCalculatorPos() {
     var height = window.innerHeight / 2 - $("#calculator").height() / 2;
     $("#calculator").css("top", height);
+}
+
+function filterInput(id, letter) {
+    var input = $("#" + id);
+    var text = input.val();
+    
+    // TO DO - FILTER KEYPRESSES
 }
